d83a28bf1594632099f39f240941a0e383dee43c
strawbreey <2675882608@qq.com>
2019-04-22 19:57:21 +0800

    fix: test

--- src/app/modal/drag-modal/drag-modal.component.ts
+++ src/app/modal/drag-modal/drag-modal.component.ts
@@ -1,40 +1,39 @@
-import {Component, EventEmitter, OnInit, Output, ViewChild} from '@angular/core';
+import {Component, EventEmitter, OnInit, Output, ViewChild, Input, ElementRef} from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { DragModalService } from './drag-modal.service';
 import { HttpClient } from '@angular/common/http';
+
+
+
+let red = 'green'
 
 
 @Component({
   selector: 'app-drag-modal',
   templateUrl: './drag-modal.component.html',
   host: {'class': 'mat-card'},
-  styles: [
-    `
-      .red {
-        color: {{this.red}}
-      }
-
-      .red {
-        color: green
-      }
-    `
-  ]
+  styleUrls: ['./drag-modal.component.css']
 })
+
+
 export class DragModalComponent implements OnInit {
 
   isVisible = true
-  red = 'red'
 
   nzZIndex = 1000
 
-  constructor(private route: ActivatedRoute, private dragService: DragModalService, private http: HttpClient, ) {
+  constructor(private route: ActivatedRoute, private dragService: DragModalService, private http: HttpClient,private el:ElementRef ) {
 
   }
 
+  @Input() hero;
+
   ngOnInit() {
+    red = 'red'
+    console.log(this.el);
     this.dragService.bulletlin$.subscribe(item => {
       this.isVisible = true
-      console.log(this)
+      // red = 'red'
     })
   }
 
